b8456408ebaf5f232bae8424207b5b2cdb924933
Andrei Babikov <andrei.babikov@gmail.com>
2022-01-30 19:57:11 +0300

    added test header helper function

--- amp360_test.go
+++ amp360_test.go
@@ -49,6 +49,12 @@ func testMethod(t *testing.T, r *http.Request, want string) {
 	}
 }
 
+func testHeader(t *testing.T, r *http.Request, header string, want string) {
+	if got := r.Header.Get(header); got != want {
+		t.Errorf("Header.Get(%q) returned %q, want %q", header, got, want)
+	}
+}
+
 func TestNewClient(t *testing.T) {
 	c := NewClient(defaultBase, nil)
 
