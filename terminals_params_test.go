c27723a1c78fc8360872e6bec2ebbe8f7b5cc536
Andrei Babikov <andrei.babikov@gmail.com>
2022-06-17 17:05:15 +0300

    fix/terminal parameters bulk update

--- terminals_params_test.go
+++ terminals_params_test.go
@@ -27,13 +27,13 @@ func TestTerminalGetParamsMock(t *testing.T) {
 			t.Errorf("Bad URL got %v", r.URL.Path)
 		} else {
 			testMethod(t, r, http.MethodGet)
-			fmt.Fprint(w, `{"success":true,"message":"Successfully found the template parameters.","data":{"categories":[{"name":"AMP Cloud","id":"6ba90b8c-0340-44fe-82c4-4bc32e51a316"},{"name":"Communications","id":"ca8958a4-8c21-4f82-af4a-58a66ac36e4a"}],"count":2,"rows":[{"id":950024,"type":"STRING","tag":"CLOUD.AUTHCODE","name":"CLOUD.AUTHCODE","hint":"","validator":"","value":"testtoken","defaultValue":"testtoken","visibleOnTemplate":1,"visibleOnTerminal":0,"filePath":"","ApplicationId":"763d0d8f-a0fd-4fa6-97e3-e44028305ba3","ParamCategoryId":"6ba90b8c-0340-44fe-82c4-4bc32e51a316","categoryName":"AMP Cloud","updatedAt":"2021-11-18T06:18:22.000Z","createdAt":"2021-11-18T06:18:22.000Z"},{"id":950047,"type":"STRING","tag":"COMMUNICATIONS.MEDIA.PRIMARY","name":"COMMUNICATIONS.MEDIA.PRIMARY","hint":"","validator":"","value":"CELLULAR","defaultValue":"CELLULAR","visibleOnTemplate":1,"visibleOnTerminal":0,"filePath":"","ApplicationId":"763d0d8f-a0fd-4fa6-97e3-e44028305ba3","ParamCategoryId":"ca8958a4-8c21-4f82-af4a-58a66ac36e4a","categoryName":"Communications","updatedAt":"2021-11-18T06:18:22.000Z","createdAt":"2021-11-18T06:18:22.000Z"}]}}`)
+			fmt.Fprint(w, `{"success":true,"message":"Successfully found the terminal parameters.","data":{"categories":[{"name":"TERMINAL","id":"c2f7c244-ebd7-4ce3-bcf6-adffd2e4ec90"}],"count":2,"rows":[{"id":3104508,"type":"STRING","tag":"ACQS._1.ACQINFO.MERCHANTID","name":"ACQS._1.ACQINFO.MERCHANTID","hint":"","validator":"","value":"400081203","defaultValue":"000000000","visibleOnTemplate":1,"visibleOnTerminal":1,"filePath":null,"ApplicationId":"c250f201-4d0b-42d3-aeb0-3c8804e4684a","ParamCategoryId":"c2f7c244-ebd7-4ce3-bcf6-adffd2e4ec90","categoryName":"TERMINAL"},{"id":3104509,"type":"STRING","tag":"ACQS._1.ACQINFO.MERCHANTNAME","name":"ACQS._1.ACQINFO.MERCHANTNAME","hint":"","validator":"","value":"MRA RESTAURANT BAKERY & SWEETS","defaultValue":"NONE","visibleOnTemplate":1,"visibleOnTerminal":1,"filePath":null,"ApplicationId":"c250f201-4d0b-42d3-aeb0-3c8804e4684a","ParamCategoryId":"c2f7c244-ebd7-4ce3-bcf6-adffd2e4ec90","categoryName":"TERMINAL"}]}}`)
 		}
 	})
 
-	tp := TemplateParams{}
+	tp := TerminalParams{}
 	wantErr := errors.New("required terminalID is missing")
-	err := c.TerminalsService.GetParams(context.Background(), "", nil, &tp)
+	err := c.TerminalsService.GetParams(context.Background(), 0, nil, &tp)
 	if err == nil {
 		t.Errorf("Error is nil, want %v", wantErr)
 	}
@@ -41,19 +41,19 @@ func TestTerminalGetParamsMock(t *testing.T) {
 		t.Errorf("Error got %v, want %v", err, wantErr)
 	}
 
-	want := 2
-	err = c.TerminalsService.GetParams(context.Background(), "814", nil, &tp)
+	want := 1
+	err = c.TerminalsService.GetParams(context.Background(), 814, nil, &tp)
 	if err != nil {
 		t.Errorf("Error occured = %v", err)
 	}
 	if len(tp.Categories) != want {
-		t.Errorf("Categorues count got %v, wnat %v", len(tp.Categories), want)
+		t.Errorf("Categories count got %v, wnat %v", len(tp.Categories), want)
 	}
-	if tp.Count != want {
-		t.Errorf("Parameters count got %v, wnat %v", len(tp.Categories), want)
+	if tp.Count != 2 {
+		t.Errorf("Parameters count got %v, wnat %v", len(tp.Categories), 2)
 	}
-	if len(tp.Rows) != want {
-		t.Errorf("Parameters actual count got %v, wnat %v", len(tp.Rows), want)
+	if len(tp.Rows) != 2 {
+		t.Errorf("Parameters actual count got %v, wnat %v", len(tp.Rows), 2)
 	}
 }
 
@@ -84,7 +84,7 @@ func TestTerminalGetParamsQueryMock(t *testing.T) {
 	opt := ParamsOpt{
 		CategoryId: "value1",
 	}
-	err := c.TerminalsService.GetParams(context.Background(), "814", opt, &tp)
+	err := c.TerminalsService.GetParams(context.Background(), 814, opt, &tp)
 	if err != nil {
 		t.Errorf("Error occured = %v", err)
 	}
@@ -124,7 +124,7 @@ func TestTerminalsUpdateParamsMock(t *testing.T) {
 	params := map[string]string{}
 	files := map[string]string{}
 
-	err := c.TerminalsService.UpdateParams(context.Background(), "", params, files, &updated, &failed)
+	err := c.TerminalsService.UpdateParams(context.Background(), 0, params, files, &updated, &failed)
 	if err == nil {
 		t.Errorf("Error is nil, want %v", wantErr)
 	}
@@ -136,7 +136,7 @@ func TestTerminalsUpdateParamsMock(t *testing.T) {
 	params["param2"] = "value2"
 	params["param3"] = "value3"
 	files["TODO"] = "./TODO"
-	err = c.TerminalsService.UpdateParams(context.Background(), "814", params, files, &updated, &failed)
+	err = c.TerminalsService.UpdateParams(context.Background(), 814, params, files, &updated, &failed)
 	if err != nil {
 		t.Errorf("Error occured = %v", err)
 	}
